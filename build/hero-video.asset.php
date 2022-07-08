180f8c6ffc92a24c6a83f730cbb2e4ca210bc112
aandrv260 <angel42505@outlook.com>
2022-07-08 11:09:47 +0300

    Added the icons component and stored certain information about the company in js files. Created page-about-us.html file.

--- build/hero-video.asset.php
+++ build/hero-video.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'be1e82ef7ac8e3f58ace');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '5cfe909582014e97d5dd');
